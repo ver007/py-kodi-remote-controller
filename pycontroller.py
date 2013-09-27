6f87f23f52aa25cbfdfc4477f426079f83857f5e
Arn-O <rnd.bertrand@gmail.com>
2013-09-27 22:20:57 +0200

    nightly build

---
+++ pycontroller.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#
+# Copyright 2013 Arn-O. See the LICENSE file at the top-level directory of this
+# distribution and at
+# https://github.com/Arn-O/py-xbmc-remote-controller/blob/master/LICENSE.
+
+'''
+XBMC remote controller based on TCP transport and JSON and using the (cmd) interface.
+'''
+
+# global constants
+
+
+def main():
+    '''Where everything starts.'''
+
+if __name__ == '__main__':
+    main()
