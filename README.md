30080bc78cb1009fcc44fdcadd287b04465e0f7e
Arn-O <rnd.bertrand@gmail.com>
2013-09-27 22:51:38 +0200

    nightly build

--- README.md
+++ README.md
@@ -8,6 +8,9 @@ Just a fun small projet to explore JSON, TCP requests, and play with my favorite
 ## Useful links
 
 + [JSON-RPC API][1]
-
++ [JSON-RPC API/Examples][2]
++ [18.2. json — JSON encoder and decoder][3]
 
 [1]: http://wiki.xbmc.org/?title=JSON-RPC_API
+[2]: http://wiki.xbmc.org/index.php?title=JSON-RPC_API/Examples
+[3]: http://docs.python.org/2/library/json.html
--- pycontroller.py
+++ pycontroller.py
@@ -8,11 +8,23 @@
 XBMC remote controller based on TCP transport and JSON and using the (cmd) interface.
 '''
 
-# global constants
+import socket
+import json
 
+# global constants
+XBMC_IP = '192.168.1.65'
+XBMC_PORT = 9090
+BUFFER_SIZE = 1024
 
 def main():
     '''Where everything starts.'''
+    command = {'jsonrpc': '2.0', 'method': 'Player.GetActivePlayers', 'id': 1}
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((XBMC_IP, XBMC_PORT))
+    s.send(json.dumps(command))
+    data = s.recv(BUFFER_SIZE)
+    s.close()
+    print data
 
 if __name__ == '__main__':
     main()
