723644279a80d46eb97660569d9080dbd6d48d1f
Arn-O <rnd.bertrand@gmail.com>
2015-07-05 09:40:26 +0200

    input kodi parameters

--- pyechonest.py
+++ pyechonest.py
@@ -5,6 +5,12 @@
 # distribution and at
 # https://github.com/Arn-O/py-kodi-remote-controller/blob/master/LICENSE.
 
+'''
+Kodi remote controller in command lines based on (cmd). Integrated with
+the echonest API for smart playlists generation.
+More info on the echonest API: http://developer.echonest.com/docs/v4
+'''
+
 import cmd
 import logging
 import argparse
@@ -29,10 +35,64 @@ def get_params():
             logger.debug('... and even in high verbosity mode!')
     return 
 
+def is_file(fname):
+    '''Return true if the file does exist'''
+    logger.debug('call function is_file')
+    try:
+        open(fname)
+    except IOError:
+        return False
+    return True
+
+def input_params():
+    '''Request the user for params input'''
+    logger.debug('call function input_params')
+    params = {}
+    params['ip'] = raw_input("Kodi server IP: ")
+    params['port'] = raw_input("Kodi server port: ")
+    params['user'] = raw_input("Kodi server user: ")
+    params['password'] = raw_input("Kodi server password: ")
+    params['echonest_key'] = raw_input("Echonest developer key: ")
+    return params
+
+def save_params(params):
+    '''Save the Kodi parameters to a local file'''
+    logger.debug('call function save_params')
+
+def display_banner():
+    '''Display initial banner'''
+    logger.debug('call function display_banner')
+    print "No Kodi params file found, this is probably the first launch. Check"
+    print "your Kodi parameters (IP, port, user and password) and create an"
+    print "Echonest account: https://developer.echonest.com/account/register"
+    print
+    print "Read the API key on the Echonest account, it will be requested"
+    print "later on. When you are ready, try params_create."
+
 class KodiRemote(cmd.Cmd):
     
     def preloop(self):
         get_params()
+        if is_file('params.pickle'):
+            logger.debug('kodi params file found')
+        else:
+            logger.info('no kodi params file')
+            print
+            display_banner()
+            print
+
+    # Kodi params file
+
+    def do_params_create(self, line):
+        '''
+        Create the Kodi params file.
+        Usage: params_create
+        '''
+        logger.debug('call function do_params_create')
+        print
+        params = input_params()
+        print
+        save_params(params)
 
     def do_EOF(self, line):
         '''Override end of file'''
